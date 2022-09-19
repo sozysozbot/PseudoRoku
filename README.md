a45f3c118f84433715dcbb3fa7eb5600cc1754ba
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2022-09-19 11:08:24 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # PseudoRoku
 議事録をいい感じにレンダリングしてくれるツール
+
+# 使用例
+[2022年セキュリティ・キャンプL3（Cコンパイラゼミ）ログ](https://sozysozbot.github.io/seccamp-2022-c-compiler-seminar/)
+
+# 使い方
+
+log_all.txt:
+
+````
+$DATE「2022年9月19日」
+阿部「ところで、進捗ってどんな感じなんです？」
+江藤「来月辺りにはリリースできそうとのことです」
+$IMAGE「sample.jpg」
+阿部「了解です。そういえば、先週あたりにこんな記事が出ていましたが、」
+$URL「https://example.com/news/baz」
+江藤「ああ、このニュースねぇ。」
+$blockquote
+```
+foo
+bar
+baz
+```
+$HTML「ということで、この件はかなり<u>重要</u>になってくると考えているんだ」
+鈴木「そうでしたか。ところで、」
+$source
+```c
+#include <stdio.h>
+int main(void) {
+	return 0;
+}
+```
+阿部「」
+$VIDEO「video.mp4」
+````
+
+censor_list.tsv:
+```tsv
+阿部	Aさん
+江藤	Eさん
+鈴木	Sさん
+```
+
+profile_lookup.tsv:
+```tsv
+Aさん	https://example.com/foo
+Eさん	https://example.com/bar
+Sさん	https://example.com/baz
+```
+
+template.html:
+```html
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>ログ</title>
+	$css
+</head>
+<body>
+<h1>ログ</h1>
+<h2>概要</h2>
+<p>このログは、かくかくしかじかの経緯でどうこう。</p>
+<h2>ログ本体</h2>$content</body>
+```
+
+```js
+new PseudoRoku({
+	input: "./log_all.txt",
+	censor_list: "./censor_list.tsv",
+	output: "docs/index.html",
+	template: './template.html',
+	profile_lookup: './profile_lookup.tsv',
+	getIconPathFromCensoredName: name => `icons/${name}.png`, // 出力された HTML からの相対パスで指定
+	getMediaPath: media => `media/${media}`, // 出力された HTML からの相対パスで指定
+}).doEverything();
+```
+
+出力された docs/index.html と同じところに icons フォルダと media フォルダを作り、icons フォルダに `Aさん.png`, `Eさん.png` を用意し、 media フォルダに `sample.jpg` や `video.mp4` を用意することで、レンダリングされる。
---
+++ src/index.ts
@@ -0,0 +1,261 @@
+import * as fs from 'fs'
+
+type Elem = { name: string, content: string, lang?: string };
+type Content = { type: "plaintext" | "url" | "blockquote" | "image" | "html" | "video", data: string } | { type: "source", data: string, lang: string };
+type Elem2 = { name: string, content: Content[] };
+
+function escapeHTML(unsafeText: string) {
+	return unsafeText.replace(
+		/[&'`"<>]/g,
+		function (match: string) {
+			return {
+				'&': '&amp;',
+				"'": '&#x27;',
+				'`': '&#x60;',
+				'"': '&quot;',
+				'<': '&lt;',
+				'>': '&gt;',
+			}[match as '&' | "'" | '`' | '"' | '<' | '>'] ?? ""
+		}
+	);
+}
+
+function assert(flag: boolean, msg: string) {
+	if (!flag) {
+		throw new Error(msg);
+	}
+}
+
+function lines_to_elems(lines: string[]): Elem[] {
+	const ans: Elem[] = [];
+	for (let i = 0; i < lines.length; i++) {
+		const line = lines[i].trim();
+		if (line === "") {
+			continue;
+		} else if (line === "$source") {
+			assert(lines[i + 1].startsWith("```"), "$source の後に ``` がない");
+			const lang = lines[i + 1].slice(3);
+			let content = "";
+			let j = i + 2;
+			for (; !lines[j].startsWith("```"); j++) {
+				content += lines[j] + "\n";
+			}
+			i = j; // the i++ in the for-loop will compensate for the closing ```
+			ans.push({ name: "$source", content, lang });
+		} else if (line === "$blockquote") {
+			assert(lines[i + 1].startsWith("```"), "$blockquote の後に ``` がない");
+			const lang = lines[i + 1].slice(3);
+			let content = "";
+			let j = i + 2;
+			for (; !lines[j].startsWith("```"); j++) {
+				content += lines[j] + "\n";
+			}
+			i = j; // the i++ in the for-loop will compensate for the closing ```
+			ans.push({ name: "$blockquote", content, lang });
+		} else {
+			const ind = line.indexOf("「");
+			const name = line.slice(0, ind);
+			assert(line.slice(-1) == "」", `${i + 1} 行目の末尾に閉じカギカッコ以外の文字がある`);
+			const content = line.slice(ind + 1, -1);
+			ans.push({ name, content })
+		}
+	}
+	return ans;
+}
+
+function group_same_person(elems: Elem[]): Elem2[] {
+	const ans: Elem2[] = [];
+	for (let i = 0; i < elems.length; i++) {
+		// same person
+		if (ans[ans.length - 1] && elems[i].name === ans[ans.length - 1].name && !elems[i].name.startsWith("$")) {
+			ans[ans.length - 1].content.push({ type: "plaintext", data: elems[i].content });
+		} else if (elems[i].name === "$URL") {
+			ans[ans.length - 1].content.push({ type: "url", data: elems[i].content });
+		} else if (elems[i].name === "$HTML") {
+			ans[ans.length - 1].content.push({ type: "html", data: elems[i].content });
+		} else if (elems[i].name === "$IMAGE") {
+			ans[ans.length - 1].content.push({ type: "image", data: elems[i].content });
+		} else if (elems[i].name === "$VIDEO") {
+			ans[ans.length - 1].content.push({ type: "video", data: elems[i].content });
+		} else if (elems[i].name === "$blockquote") {
+			ans[ans.length - 1].content.push({ type: "blockquote", data: elems[i].content });
+		} else if (elems[i].name === "$source") {
+			ans[ans.length - 1].content.push({ type: "source", data: elems[i].content, lang: elems[i].lang ?? "" });
+		} else {
+			ans.push({ name: elems[i].name, content: [{ type: "plaintext", data: elems[i].content }] });
+		}
+	}
+	return ans;
+}
+
+// copied and modified from https://codepremix.com/detect-urls-in-text-and-create-a-link-in-javascript
+function replaceURLs(message: string): string {
+	if (!message) return "";
+
+	var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+	return message.replace(urlRegex, function (url: string) {
+		var hyperlink = url;
+		if (!hyperlink.match('^https?:\/\/')) {
+			hyperlink = 'http://' + hyperlink;
+		}
+		return '<a href="' + hyperlink + '" target="_blank" rel="noopener noreferrer">' + url + '</a>'
+	});
+}
+
+export class PseudoRoku {
+	input: string;
+	censor_list: string | undefined;
+	output: string;
+	profile_lookup: string;
+	template: string;
+	getIconPathFromCensoredName: (censored_name: string) => string;
+	getMediaPath: (media: string) => string = (media: string) => `media/${media}`;
+
+	constructor(o: {
+		input: string,
+		censor_list?: string,
+		output: string,
+		profile_lookup: string,
+		template: string,
+		getIconPathFromCensoredName: (censored_name: string) => string,
+		getMediaPath?: (media: string) => string,
+	}) {
+		this.input = o.input;
+		this.censor_list = o.censor_list;
+		this.output = o.output;
+		this.profile_lookup = o.profile_lookup;
+		this.template = o.template;
+		this.getIconPathFromCensoredName = o.getIconPathFromCensoredName;
+		if (o.getMediaPath) {
+			this.getMediaPath = o.getMediaPath;
+		}
+	}
+
+	public doEverything() {
+		const data = fs.readFileSync(this.input, { encoding: 'utf8', flag: 'r' });
+		const lines = data.split("\n");
+
+		const censor_table = !this.censor_list ? null : fs.readFileSync(this.censor_list, { encoding: 'utf8', flag: 'r' }).split("\n").map(a => a.split("\t"));
+		const profile_lookup_table = fs.readFileSync(this.profile_lookup, { encoding: 'utf8', flag: 'r' }).split("\n").map(a => a.split("\t"));
+
+		function censor(unsafeText: string) {
+			if (!censor_table) return unsafeText;
+			for (let i = 0; i < censor_table.length; i++) {
+				unsafeText = unsafeText.replace(new RegExp(censor_table[i][0], "g"), censor_table[i][1].trim());
+			}
+			return unsafeText;
+		}
+
+		function elems_to_html(elems: Elem2[]): string {
+			let ans = ``;
+			for (let i = 0; i < elems.length; i++) {
+				if (elems[i].name === "$DATE") {
+					ans += `<h3 class="date">${escapeHTML(censor(elems[i].content[0].data))}</h3>\n`;
+				} else {
+					ans += `<div class="one_person">\n`;
+					if (elems[i].name.startsWith("$JOIN")) {
+						// "$JOIN{foo}{bar}{baz}"
+						// ↓ .slice
+						// "foo}{bar}{baz"
+						// ↓ .split
+						// ["foo", "bar", "baz"]
+						const names = elems[i].name.slice("$JOIN{".length, -1).split("}{");
+						ans += "\t" + names.map(n => getLinkedIconFromUncensoredName(n, 48 / names.length)).join("") + `
+	<div class="name_and_content">
+		<span class="name">${names.length > 5 ? "一同" : names.map(n => escapeHTML(censor(n))).join("・")}</span>
+		${elems[i].content.map(renderContent).join("\n\t\t")}
+	</div>`;
+					} else {
+						ans += `\t${getLinkedIconFromUncensoredName(elems[i].name, 48)}
+	<div class="name_and_content">
+		${getLinkedNameFromUncensoredName(elems[i].name)}
+		${elems[i].content.map(renderContent).join("\n\t\t")}
+	</div>`;
+					}
+					ans += `\n</div>\n`;
+				}
+			}
+			return ans;
+		}
+
+		function getLinkedNameFromUncensoredName(name: string) {
+			const profileURL = profile_lookup_table.filter(k => k[0] === escapeHTML(censor(name)))[0]?.[1].trim();
+			if (!profileURL) {
+				return `<span class="name">${escapeHTML(censor(name))}</span>`
+			} else {
+				return `<a class="name" href="${profileURL}" target="_blank" rel="noopener noreferrer">${escapeHTML(censor(name))}</a>`
+			}
+		}
+
+		const getLinkedIconFromUncensoredName = (name: string, size: number): string => {
+			const profileURL = profile_lookup_table.filter(k => k[0] === escapeHTML(censor(name)))[0]?.[1].trim();
+			const img = `<img alt="${escapeHTML(censor(name))}" class="icon" src="${this.getIconPathFromCensoredName(censor(name))}" height="${size}px">`;
+			if (!profileURL) { // either undefined or empty
+				return img;
+			} else {
+				return `<a href="${profileURL}" target="_blank" rel="noopener noreferrer">${img}</a>`
+			}
+		}
+
+		const renderContent: (e: Content) => string = (e: Content) => {
+			const censored = censor(e.data);
+			if (e.type === "url") {
+				return `<div class="content"><a href="${escapeHTML(censored)}" target="_blank" rel="noopener noreferrer">${escapeHTML(censored)}</a></div>`;
+			} else if (e.type === "plaintext") {
+				return `<div class="content">${replaceURLs(escapeHTML(censored))}</div>`;
+			} else if (e.type === "html") {
+				return `<div class="content">${censored}</div>`;
+			} else if (e.type === "blockquote") {
+				return `<blockquote>${replaceURLs(escapeHTML(censored))}</blockquote>`;
+			} else if (e.type === "image") {
+				return `<div class="content"><img width="500" src="${this.getMediaPath(escapeHTML(censored))}"></div>`;
+			} else if (e.type === "source") {
+				// CSS class here is the format expected by [highlight.js](https://highlightjs.org/),
+				// though the use of highlight.js is by no means necessary.
+				return `<pre><code class="language-${e.lang}">${escapeHTML(censored)}</code></pre>`;
+			} else if (e.type === "video") {
+				return `<div class="content"><video controls width="500" src="${this.getMediaPath(escapeHTML(censored))}"></video></div>`;
+			} else {
+				const _: never = e.type;
+				throw new Error("Should not reach here: unexpected Content.type");
+			}
+		}
+
+		fs.writeFileSync(this.output,
+			fs.readFileSync(this.template, { encoding: 'utf8', flag: 'r' })
+				.replace(/\$css/g, `<style>
+		img.icon {
+			border-radius: 50%;
+		}
+		div.one_person {
+			display: flex;
+			padding-bottom: 10px;
+		}
+		blockquote {
+			position: relative;
+			border-left: 3px solid #005242;
+			padding-left: 10px;
+			margin-inline-start: 10px;
+			white-space: pre-wrap;
+		}
+		a {
+			overflow-wrap: anywhere;
+		}
+		h3 {
+			margin-top: 15px;
+		}
+		a.name {
+			text-decoration: none;
+			color: inherit;
+		}
+		a.name:hover {
+			text-decoration: underline;
+		}
+		div.name_and_content {
+			padding-left: 10px;
+		}
+	</style>`)
+				.replace(/\$content/g, elems_to_html(group_same_person(lines_to_elems(lines))))
+		);
+	}
+}
